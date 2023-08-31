55158aba11901160b5ba699bff318b433b1d2b36
matheus1714 <matheussilvamartins1714@gmail.com>
2023-08-31 15:12:35 -0300

    config grid

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,18 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
-  content: [
-    './src/**/*.tsx',
-  ],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
-      maxWidth:{
-        app: '700px'
+      gridTemplateColumns:{
+        app:'minmax(18rem, 20rem) 1fr'
+      },
+      maxWidth: {
+        app: '700px',
       },
       colors: {
         diego: '#8257e5',
-      }
+      },
     },
   },
   plugins: [],
